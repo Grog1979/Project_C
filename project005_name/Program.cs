c5621676b065184ef8c69c08f1727784a57f148c
Kostya <kosg@mail.ru>
2023-03-08 23:35:31 +0300

    add name

---
+++ project005_name/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("ВВЕДИТЕ ИМЯ ПОЛЬЗОВАТЕЛЯ: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "masha")
+{
+    Console.WriteLine("Hello Masha");
+}
+else
+{
+    Console.Write("Hi, ");
+    Console.WriteLine(username);
+}
